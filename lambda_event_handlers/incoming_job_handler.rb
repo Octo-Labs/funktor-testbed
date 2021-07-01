7be27dd6b8a7ea1afbd995777561b868abc5f94a
Jeremy Green <jeremy@octolabs.com>
2021-06-30 20:34:07 -0500

    funktor init, and pick changes

--- lambda_event_handlers/incoming_job_handler.rb
+++ lambda_event_handlers/incoming_job_handler.rb
@@ -1,3 +1,6 @@
+# For this handler we don't need to know about your app, or any of the other gems,
+# so instead of doing `require_relative '../config/boog'` we just manually require
+# the one gem that we do need.
 require 'funktor'
 
 $handler = Funktor::IncomingJobHandler.new
