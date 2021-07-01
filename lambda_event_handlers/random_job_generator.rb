dee032f05695a88d994a9674ea7301a88317a7d1
Jeremy Green <jeremy@octolabs.com>
2021-06-30 21:21:10 -0500

    Get the job rate working correctly

--- lambda_event_handlers/random_job_generator.rb
+++ lambda_event_handlers/random_job_generator.rb
@@ -7,11 +7,11 @@ class RandomJobGenerator
 
   def self.call(event:, context:)
     job_count = 0
+    jobs_to_generate = rand(ENV['MIN_RANDOM_JOBS_PER_MINUTE'].to_i..ENV['MAX_RANDOM_JOBS_PER_MINUTE'].to_i)
+    sleep_delay = 53.0/jobs_to_generate # Calculate our sleep based on the target rates
+    puts "jobs_to_generate = #{jobs_to_generate} and sleep_delay = #{sleep_delay}"
     while context.get_remaining_time_in_millis > 5_000 do # This lets us exit gracefully and resume on the next round instead of getting forcibly killed.
       delay = rand(0..ENV['MAX_JOB_DELAY_IN_SECONDS'].to_f) # Schedule the job for sometime between now and MAX_JOB_DELAY
-      max_sleep_delay = 60.0/ENV['MIN_RANDOM_JOBS_PER_MINUTE'].to_f
-      min_sleep_delay = 60.0/ENV['MAX_RANDOM_JOBS_PER_MINUTE'].to_f
-      sleep_delay = rand(min_sleep_delay..max_sleep_delay) # Calculate our sleep based on the target rates
       job_sleep = rand(0..ENV['MAX_JOB_LENGTH_IN_SECONDS'].to_f) # Then sleep for up to MAX_SLEEP_DELAY
       error_percentage = ENV['ERROR_PERCENTAGE'].to_i
 
